6a4e6b934e5fde3e8e6cb3a6dc0487f1e64959e2
SreelakshmiVK <vksreelakshmi@yahoo.co.in>
2016-07-14 12:55:03 +0530

    Remove unwanted comments

--- tts_converter/tts_converter.py
+++ tts_converter/tts_converter.py
@@ -39,21 +39,12 @@ class Tts_converter :
 
 #take command line args as the input string
 tts_Obj = Tts_converter(sys.argv)
-#input_string = sys.argv
 
-#remove the program name from the argv list
-#input_string.pop(0)
-#print 'File name removed : ' + str(tts_Obj.input_string)
-
-#input_file = input_string.pop()
 print 'Input File name is :' + tts_Obj.input_file
 
 file_desc = open(tts_Obj.input_file,'r')
-#print 'File Descriptor is : ' + str(file_desc)
 
 file_content = tts_Obj.file_Read(file_desc)
-#if file_content == '' :
-#	break
 
 while file_content != '' :
 
@@ -68,21 +59,8 @@ while file_content != '' :
 	tts_string = tts_Obj.tts_String(file_content)
 	music_stream_uri = tts_Obj.music_Stream_Gen(tts_string)
 	tts_Obj.play_Speech(music_stream_uri)
-#convert to google friendly url (with + replacing spaces)
-#tts_string = '+'.join(file_content.split())
-#		print tts_string
-
-		#use string in combination with the translate url as the stream to be played
-#		music_stream_uri = 'https://translate.google.com/translate_tts?ie=UTF-8&tl=en&q='
-#		music_stream_uri += tts_string + "&client=tw-ob"
-#		print  music_stream_uri
-
-#		player = gst.element_factory_make("playbin","player")
-#		player.set_property('uri',music_stream_uri)
-#		player.set_state(gst.STATE_PLAYING)
 	
-
-		#requires a delay, if the py process closes before the mp3 has finished it will be cut off.
+	#requires a delay, if the py process closes before the mp3 has finished it will be cut off.
 	time.sleep(7)
 	file_content = tts_Obj.file_Read(file_desc)
 	
